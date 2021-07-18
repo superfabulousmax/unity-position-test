28e73e703ed9fa7989b4e6718691d2e1b5e0ea20
superfabulousmax <sinzara611@gmail.com>
2021-07-18 12:47:13 +0100

    Add tests for every symbol vs each other and check correct outcome is sent

--- Assets/Project/Scripts/Tests/Play/SymbolOutcomesTest.cs
+++ Assets/Project/Scripts/Tests/Play/SymbolOutcomesTest.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System;
 using Zenject;
+using static SymbolOutcomeCalculator;
 
 namespace Project.Tests.Play
 {
@@ -21,26 +22,92 @@ namespace Project.Tests.Play
 		[Test]
 		public void TestRockOutCome()
 		{
-			TestOutcome(SymbolOutcomeCalculator.Symbols.Rock, typeof(RockOutcomeCaculator));
+			TestOutcome(Symbols.Rock, typeof(RockOutcomeCaculator));
 		}
 
 		[Test]
 		public void TestPaperOutCome()
 		{
-			TestOutcome(SymbolOutcomeCalculator.Symbols.Paper, typeof(PaperOutcomeCaculator));
+			TestOutcome(Symbols.Paper, typeof(PaperOutcomeCaculator));
 		}
 
 		[Test]
 		public void TestScissorsOutCome()
 		{
-			TestOutcome(SymbolOutcomeCalculator.Symbols.Scissors, typeof(ScissorsOutcomeCaculator));
+			TestOutcome(Symbols.Scissors, typeof(ScissorsOutcomeCaculator));
 		}
 
-		public void TestOutcome(SymbolOutcomeCalculator.Symbols symbol, Type expectedType)
+		public void TestOutcome(Symbols symbol, Type expectedType)
 		{
 			var outcomeCalculator = symbolOutcomeFactory.Create((uint)symbol);
 			Assert.True(outcomeCalculator != null, $"Check that the {symbol} symbol is not null");
 			Assert.That(outcomeCalculator.GetType(), Is.EqualTo(expectedType));
+		}
+
+		[Test]
+		public void TestRockVsScissors()
+		{
+			TestVersus(Symbols.Rock, Symbols.Scissors, "Player Win!");
+		}
+
+		[Test]
+		public void TestScissorsVsPaper()
+		{
+			TestVersus(Symbols.Scissors, Symbols.Paper, "Player Win!");
+		}
+
+		[Test]
+		public void TestPaperVsRock()
+		{
+			TestVersus(Symbols.Paper, Symbols.Rock, "Player Win!");
+		}
+
+		[Test]
+		public void TestScissorsVsRock()
+		{
+			TestVersus(Symbols.Scissors, Symbols.Rock, "AI Win!");
+		}
+
+		[Test]
+		public void TestPaperVsScissors()
+		{
+			TestVersus(Symbols.Paper, Symbols.Scissors, "AI Win!");
+		}
+
+		[Test]
+		public void TestRockVsPaper()
+		{
+			TestVersus(Symbols.Rock, Symbols.Paper, "AI Win!");
+		}
+
+		[Test]
+		public void RockVsRock()
+		{
+			TestVersus(Symbols.Rock, Symbols.Rock, "Tie!");
+		}
+
+		[Test]
+		public void PaperVsPaper()
+		{
+			TestVersus(Symbols.Paper, Symbols.Paper, "Tie!");
+		}
+
+		[Test]
+		public void ScissorsVsScissors()
+		{
+			TestVersus(Symbols.Scissors, Symbols.Scissors, "Tie!");
+		}
+
+
+		public void TestVersus(Symbols symbol1, Symbols symbol2, string message)
+		{
+			var outcomeCalculator1 = symbolOutcomeFactory.Create((uint)symbol1);
+			var outcomeCalculator2 = symbolOutcomeFactory.Create((uint)symbol2);
+			outcomeCalculator1.OutcomeDetermined += delegate (object sender, OutcomeDeterminedEventArgs args)
+			{
+				Assert.That(message, Is.EqualTo(args.outcomeMessage));
+			};
+			outcomeCalculator1.DetermineOutcome(outcomeCalculator2);
 		}
 	}
 }
